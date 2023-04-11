54da8ec21e91045aba907e5394d1696879b47308
Mushroomator <ThomasPilz@gmx.de>
2023-04-11 20:30:15 +0200

    Use pointers and adjust variable naming

--- unboundedNonDeterminism/main.go
+++ unboundedNonDeterminism/main.go
@@ -19,15 +19,17 @@ func main() {
 	// create actor system
 	system := actor.NewActorSystem()
 	// spawn an actor
-	actorProps := actor.PropsFromProducer(func() actor.Actor { return &UnboundedNonDetActor{counter: 0} })
-	pidUndActor := system.Root.Spawn(actorProps)
+	actorProps := actor.PropsFromProducer(func() actor.Actor {
+		return &UnboundedNonDetActor{counter: 0}
+	})
+	pid := system.Root.Spawn(actorProps)
 
 	sendMessage := func(msg interface{}) {
-		system.Root.Send(pidUndActor, msg)
+		system.Root.Send(pid, msg)
 	}
 	// send continue and stop message concurrently
-	go sendMessage(Continue{})
-	go sendMessage(Stop{})
+	go sendMessage(&Continue{})
+	go sendMessage(&Stop{})
 
 	bufio.NewScanner(os.Stdin).Scan()
 }
@@ -36,12 +38,12 @@ func (state *UnboundedNonDetActor) Receive(ctx actor.Context) {
 	switch ctx.Message().(type) {
 	case *actor.Started:
 		fmt.Println("Actor has started...")
-	case Continue:
+	case *Continue:
 		// increase counter
 		state.counter++
 		// sends itself a continue message
-		ctx.Send(ctx.Self(), Continue{})
-	case Stop:
+		ctx.Send(ctx.Self(), &Continue{})
+	case *Stop:
 		fmt.Printf("Actor has counted up to %v.\n", state.counter)
 		// kills itself after this message has been processed
 		ctx.Poison(ctx.Self())
